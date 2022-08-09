25b5436a40ce3ca667bc8e1008668cc5d2529253
Gabb-c <gdgabrieldacunha71@gmail.com>
2022-08-09 02:27:19 -0300

    chore: change to https

--- src/utils/api/api-instance.ts
+++ src/utils/api/api-instance.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const API_CLIENT = axios.create({
-  baseURL: 'http://5c4b2a47aa8ee500142b4887.mockapi.io/api/v1/dragon',
+  baseURL: 'https://5c4b2a47aa8ee500142b4887.mockapi.io/api/v1/dragon',
 });
