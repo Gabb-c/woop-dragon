05af986b37cc81e5017373b4177d9250e5213901
Gabb-c <gdgabrieldacunha71@gmail.com>
2022-08-09 04:27:53 -0300

    test(components): add textarea unit test

--- src/__tests__/components/text-area.spec.tsx
+++ src/__tests__/components/text-area.spec.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
-import { TextField } from '../../components/TextField';
+import { TextArea } from '../../components/TextArea';
 
-describe('TextField', () => {
+describe('Button', () => {
   it('test if it renders in the DOM', () => {
-    render(<TextField label="Test" />);
+    render(<TextArea label="Test" />);
 
-    const resTest = screen.getByTestId(/input-test/i);
+    const resTest = screen.getByTestId(/textarea-test/i);
 
     expect(resTest).toBeInTheDocument();
   });
@@ -18,9 +18,9 @@ describe('TextField', () => {
       preventDefault: jest.fn(),
     };
 
-    render(<TextField label="Test" onChange={() => mock} />);
+    render(<TextArea label="Test" onChange={() => mock} />);
 
-    const resTest = screen.getByTestId(/input-test/i);
+    const resTest = screen.getByTestId(/textarea-test/i);
     await userEvent.type(resTest, 'test');
 
     await waitFor(() => {
@@ -29,9 +29,9 @@ describe('TextField', () => {
   });
 
   it('test if it matches snapshot', () => {
-    render(<TextField label="Test" />);
+    render(<TextArea label="Test" />);
 
-    const resTest = screen.getByTestId(/input-test/i);
+    const resTest = screen.getByTestId(/textarea-test/i);
 
     expect(resTest).toMatchSnapshot();
   });
