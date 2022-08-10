84bfcea3a293f880de01e5ad1d8df8b7dce0dfe1
Gabb-c <gdgabrieldacunha71@gmail.com>
2022-08-10 02:54:46 -0300

    fix(pages): replac svg for component

--- src/pages/Dragons/components/DragonList/index.tsx
+++ src/pages/Dragons/components/DragonList/index.tsx
@@ -6,6 +6,7 @@ import { DragonCard } from '../DragonCard';
 import { useEffect, useMemo, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { TextField } from '@components/TextField';
+import { Loading } from '@components/Loading';
 
 export const DragonList: React.FC = (): JSX.Element => {
   const [list, setList] = useState<Dragon[]>([]);
@@ -58,74 +59,7 @@ export const DragonList: React.FC = (): JSX.Element => {
         onChange={({ target: { value } }) => setFilter(value)}
       />
       {loading ? (
-        <>
-          <svg width="200px" height="200px" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid">
-            <rect x="17.5" y="30" width="15" height="40" fill="#646cff">
-              <animate
-                attributeName="y"
-                repeatCount="indefinite"
-                dur="1s"
-                calcMode="spline"
-                keyTimes="0;0.5;1"
-                values="18;30;30"
-                keySplines="0 0.5 0.5 1;0 0.5 0.5 1"
-                begin="-0.2s"
-              ></animate>
-              <animate
-                attributeName="height"
-                repeatCount="indefinite"
-                dur="1s"
-                calcMode="spline"
-                keyTimes="0;0.5;1"
-                values="64;40;40"
-                keySplines="0 0.5 0.5 1;0 0.5 0.5 1"
-                begin="-0.2s"
-              ></animate>
-            </rect>
-            <rect x="42.5" y="30" width="15" height="40" fill="#646cff">
-              <animate
-                attributeName="y"
-                repeatCount="indefinite"
-                dur="1s"
-                calcMode="spline"
-                keyTimes="0;0.5;1"
-                values="20.999999999999996;30;30"
-                keySplines="0 0.5 0.5 1;0 0.5 0.5 1"
-                begin="-0.1s"
-              ></animate>
-              <animate
-                attributeName="height"
-                repeatCount="indefinite"
-                dur="1s"
-                calcMode="spline"
-                keyTimes="0;0.5;1"
-                values="58.00000000000001;40;40"
-                keySplines="0 0.5 0.5 1;0 0.5 0.5 1"
-                begin="-0.1s"
-              ></animate>
-            </rect>
-            <rect x="67.5" y="30" width="15" height="40" fill="#646cff">
-              <animate
-                attributeName="y"
-                repeatCount="indefinite"
-                dur="1s"
-                calcMode="spline"
-                keyTimes="0;0.5;1"
-                values="20.999999999999996;30;30"
-                keySplines="0 0.5 0.5 1;0 0.5 0.5 1"
-              ></animate>
-              <animate
-                attributeName="height"
-                repeatCount="indefinite"
-                dur="1s"
-                calcMode="spline"
-                keyTimes="0;0.5;1"
-                values="58.00000000000001;40;40"
-                keySplines="0 0.5 0.5 1;0 0.5 0.5 1"
-              ></animate>
-            </rect>
-          </svg>
-        </>
+        <Loading />
       ) : (
         <div className={styles.lisCtontainer}>
           {filtered.length > 0
