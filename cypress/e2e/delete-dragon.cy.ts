578e690352dd8a5efad157b2dae583d2f495ca06
Gabb-c <gdgabrieldacunha71@gmail.com>
2022-08-11 04:42:20 -0300

    test(e2e): add validation if there is no dragons in the page

--- cypress/e2e/delete-dragon.cy.ts
+++ cypress/e2e/delete-dragon.cy.ts
@@ -3,7 +3,18 @@
 describe('Delete Dragon', () => {
   it('should click in the add button, fill the form and submit', () => {
     cy.login();
-    cy.wait(2000);
+
+    cy.intercept('GET', 'https://5c4b2a47aa8ee500142b4887.mockapi.io/api/v1/dragon').as(
+      'getAllDragons'
+    );
+
+    cy.wait('@getAllDragons').then((int) => {
+      if (int.response.body.length === 0) {
+        cy.addManyDragons();
+        cy.reload();
+        cy.wait(1000);
+      }
+    });
 
     cy.get('button').contains('Delete').click();
   });
