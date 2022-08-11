8c98c20ecf097a500d79b0de3c89bd6cb617bfb5
Gabb-c <gdgabrieldacunha71@gmail.com>
2022-08-11 13:06:33 -0300

    test(e2e): wait doesnt quit

--- cypress/e2e/delete-dragon.cy.ts
+++ cypress/e2e/delete-dragon.cy.ts
@@ -4,17 +4,17 @@ describe('Delete Dragon', () => {
   it('should click in the add button, fill the form and submit', () => {
     cy.login();
 
-    cy.intercept('GET', 'https://5c4b2a47aa8ee500142b4887.mockapi.io/api/v1/dragon').as(
-      'getAllDragons'
-    );
+    // cy.intercept('GET', 'https://5c4b2a47aa8ee500142b4887.mockapi.io/api/v1/dragon').as(
+    //   'getAllDragons'
+    // );
 
-    cy.wait('@getAllDragons').then((int) => {
-      if (int.response.body.length === 0) {
-        cy.addManyDragons();
-        cy.reload();
-        cy.wait(1000);
-      }
-    });
+    // cy.wait('@getAllDragons').then((int) => {
+    //   if (int.response.body.length === 0) {
+    //     cy.addManyDragons();
+    //     cy.reload();
+    //     cy.wait(1000);
+    //   }
+    // });
 
     cy.get('button').contains('Delete').click();
   });
