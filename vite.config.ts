eb6e0fa2319fabe5a86cb3e14f3520746208bc2c
Gabb-c <gdgabrieldacunha71@gmail.com>
2022-08-11 02:12:50 -0300

    build(deps): add vite pwa plugin

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import svgr from 'vite-plugin-svgr';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), svgr()],
+  plugins: [react(), tsconfigPaths(), svgr(), VitePWA({ registerType: 'autoUpdate' })],
 });
